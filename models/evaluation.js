c1ef04af848b11379abce462edc188c12542334a
haripriya <haripriya27@gmail.com>
2023-04-04 15:27:03 +0530

    label-studio

--- models/evaluation.js
+++ models/evaluation.js
@@ -1,45 +1,67 @@
 var mongoose = require("mongoose");
 
-const evaluation=mongoose.Schema({
-    xray_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"xray",
-        
-    },
-    ai_identified_cavities:{
-        type:Number,
-        default:null
-    },
-    dentist_correction:{
-      type: [{
-        x:Number,
-        y:Number
-      }],
-    
-    },
-    admin_correction:{
-        type: [{
-          x:Number,
-          y:Number
-        }],
-      
+const evaluation = mongoose.Schema({
+  xray_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "xray",
+
+  },
+  ai_identified_cavities: {
+    type: Number,
+    default: null
+  },
+  dentist_correction: {
+    type: [{
+      id: String,
+      original_height:Number,
+      original_width:Number,
+      value: {
+        ellipselabels: Array,
+        radiusX: Number,
+        radiusY: Number,
+        rotation: Number,
+        x: Number,
+        y: Number
       },
-    dentist_correction_percentage:{
-        type:Number,
-        default:null
-    },
-    admin_correction_percentage:{
-        type:Number
-    },
-    evaluated_by: {
-        type: mongoose.Schema.Types.ObjectId,
-        default: undefined,
-        ref:"User"
-    },
-    evaluated_on: {
-        type: Date,
-    
-    },
+
+    }],
+
+  },
+  admin_correction: {
+    type: [{
+      id: String,
+      original_height:Number,
+      original_width:Number,
+      value: {
+        ellipselabels: Array,
+        radiusX: Number,
+        radiusY: Number,
+        rotation: Number,
+        x: Number,
+        y: Number
+      },
+
+    }],
+
+  },
+  dentist_correction_percentage: {
+    type: Number,
+    default: null
+  },
+  admin_correction_percentage: {
+    type: Number
+  },
+  evaluated_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: undefined,
+    ref: "User"
+  },
+  evaluated_on: {
+    type: Date,
+
+  },
 })
 
-module.exports=mongoose.model("evaluation",evaluation);
+module.exports = mongoose.model("evaluation", evaluation);
+
+
